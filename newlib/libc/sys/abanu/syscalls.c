3c07b17201fdd198ebd45178c5f7ac618521c619
Sebastian Loncar <sebastian.loncar@gmail.com>
2019-11-23 14:47:50 +0100

    sbrk & write

--- newlib/libc/sys/abanu/syscalls.c
+++ newlib/libc/sys/abanu/syscalls.c
@@ -13,7 +13,7 @@ void _exit()
 
 int close(int file)
 {
-    return 0;
+	return 0;
 }
 
 char **environ; /* pointer to array of char * strings that define the current environment variables */
@@ -22,27 +22,42 @@ int execve(char *name, char **argv, char **env);
 
 int fork();
 
-__attribute__ ((noinline)) int _SCall_(int target, int arg1)
+__attribute__((noinline)) int __SysCall1(int target, int arg1)
 {
-    int ret;
-    __asm__ volatile (
-        "int $250;"
-        : "=a" (ret)
-        : "a" ((long)(target)), "b" (arg1)
-    );
-    return ret;
+	int ret;
+	__asm__ volatile(
+		 "int $250;"
+		 : "=a"(ret)
+		 : "a"((long)(target)), "b"(arg1)
+		 : "%ecx", "%edx", "%edi", "%esi");
+	return ret;
 }
 
-__attribute__ ((noinline)) int fstat(int file, struct stat *st)
+__attribute__((noinline)) int __SysCall2(int target, int arg1, int arg2)
 {
-    return _SCall_(88, 89);
+	int ret;
+	__asm__ volatile(
+		 "int $250;"
+		 : "=a"(ret)
+		 : "a"((int)(target)), "b"(arg1), "c"(arg2)
+		 : "%edx", "%edi", "%esi");
+	return ret;
+}
+
+__attribute__((noinline)) int fstat(int file, struct stat *st)
+{
+	//void *addr = __SysCall2(20, 4096, 4); // 4 = basic
+	//int result = __SysCall2(35, file, (int)addr);
+	//*st = *((struct stat *)addr);
+	//return result;
+	return 0;
 }
 
 int getpid();
 
 int isatty(int file)
 {
-    return 0;
+	return 0;
 }
 
 int kill(int pid, int sig);
@@ -51,19 +66,20 @@ int link(char *old, char *new);
 
 int lseek(int file, int ptr, int dir)
 {
-    return 0;
+	return 0;
 }
 
 int open(const char *name, int flags, ...);
 
 int read(int file, char *ptr, int len)
 {
-    return 0;
+	return 0;
 }
 
 caddr_t sbrk(int incr)
 {
-    return (caddr_t)0;
+	void *addr = (void *)__SysCall1(24, incr); // 4 = basic
+	return (caddr_t)addr;
 }
 
 int stat(const char *file, struct stat *st);
@@ -78,6 +94,16 @@ int wait(int *status)
 
 int write(int file, char *ptr, int len)
 {
+	char *addr = __SysCall2(20, 4096, 4); // 4 = basic
+
+	int i;
+	for (i = 0; i < len; i++)
+	{
+		addr[i] = ptr[i];
+	}
+
+	int result = __SysCall2(32, file, (int)addr);
+	return len;
 }
 
 //int gettimeofday(struct timeval *p, struct timezone *z);
